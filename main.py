f14b6dc053c1fc8acc9e74c81069433fbb170f11
OMalenivskyi <s01081995@gmail.com>
2024-02-15 22:45:03 +0200

    Task-2, Completed!

--- main.py
+++ main.py
@@ -14,4 +14,20 @@ elif choice == "avg":
     result = (num1 + num2 + num3) / 3
     print("Average:", result)
 else:
-    print("Incorrect operator, try again with the next operator: 'max', 'min', 'avg'.")
+    print("Incorrect operator, try again with the next operator: 'max', 'min', 'avg'."
+###
+meters = float(input("Enter meter quantity: "))
+
+choice = input("Choose an operation (miles, inches or yards): ")
+
+if choice == "miles":
+    result = meters * 0.000621371
+    print("Quantity miles: ", result)
+elif choice == "inches":
+    result = meters * 39.3701
+    print("Quantity inches: ", result)
+elif choice == "yards":
+    result = meters * 1.09361
+    print("Quantity yards: ", result)
+else:
+    print("Incorrect operation selection. Please select 'miles', 'inches' or 'yards'.")
