3a6aea4b6a6285db293deb918c0c8b43ba15e684
OMalenivskyi <s01081995@gmail.com>
2024-09-14 16:14:24 +0300

    lesson_10_t_3 - new commit in the new group

--- lesson_10_t3.py
+++ lesson_10_t3.py
@@ -11,7 +11,7 @@ def is_even(digit):
     """
     return digit % 2 == 0
 
-assert is_even(2) == True, 'Test1'
+assert is_even(6) == True, 'Test1'
 assert is_even(5) == False, 'Test2'
-assert is_even(0) == True, 'Test3'
+assert is_even(2.0) == True, 'Test3'
 print('OK')
